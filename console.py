45f6b1c3abc1ea69dd2de989d629143a280ba346
MrNeonn75 <devneonn75@gmail.com>
2024-04-02 03:24:39 +0400

    Bug fix

--- console.py
+++ console.py
@@ -113,29 +113,25 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, arg):
-        """Create a new instance of a given class."""
-        if not arg:
+    def do_create(self, args):
+        """ Create an object of any class"""
+        try:
+            if not args:
+                raise SyntaxError()
+            arg_list = args.split(" ")
+            kw = {}
+            for arg in arg_list[1:]:
+                arg_splited = arg.split("=")
+                arg_splited[1] = eval(arg_splited[1])
+                if type(arg_splited[1]) is str:
+                    arg_splited[1] = arg_splited[1].replace("_", " ")
+                    arg_splited[1] = arg_splited[1].replace('"', '\\"')
+                kw[arg_splited[0]] = arg_splited[1]
+        except SyntaxError:
             print("** class name missing **")
-            return
-
-        args = arg.split()
-        classname = args[0]
-
-        if classname not in models.classes.keys():
+        except NameError:
             print("** class doesn't exist **")
-            return
-
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-
-        new_instance = models.classes[classname]()
-        for pair in args[1:]:
-            key, value = pair.split('=')
-            if hasattr(new_instance, key):
-                value = value.strip('"')
-                setattr(new_instance, key, value)
+        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
         new_instance.save()
         print(new_instance.id)
 
--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -17,3 +17,41 @@ CREATE TABLE IF NOT EXISTS cities (
     state_id VARCHAR(60),
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
+
+CREATE TABLE IF NOT EXISTS places (
+    id VARCHAR(60) PRIMARY KEY,
+    city_id VARCHAR(60),
+    user_id VARCHAR(60),
+    name VARCHAR(128) NOT NULL,
+    description VARCHAR(1024),
+    number_rooms INTEGER NOT NULL DEFAULT 0,
+    number_bathrooms INTEGER NOT NULL DEFAULT 0,
+    max_guest INTEGER NOT NULL DEFAULT 0,
+    price_by_night INTEGER NOT NULL DEFAULT 0,
+    latitude FLOAT,
+    longitude FLOAT,
+    FOREIGN KEY (city_id) REFERENCES cities(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS amenities (
+    id VARCHAR(60) PRIMARY KEY,
+    name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS place_amenity (
+    place_id VARCHAR(60),
+    amenity_id VARCHAR(60),
+    PRIMARY KEY (place_id, amenity_id),
+    FOREIGN KEY (place_id) REFERENCES places(id),
+    FOREIGN KEY (amenity_id) REFERENCES amenities(id)
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id VARCHAR(60) PRIMARY KEY,
+    text VARCHAR(1024) NOT NULL,
+    place_id VARCHAR(60),
+    user_id VARCHAR(60),
+    FOREIGN KEY (place_id) REFERENCES places(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
