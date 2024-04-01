672fbfbd655be4029fee0897587c042291ac56e9
MrNeonn75 <devneonn75@gmail.com>
2024-04-02 01:11:17 +0400

    6. DBStorage - States and Cities Bug fix

--- models/state.py
+++ models/state.py
@@ -14,10 +14,15 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    cities = relationship("City", backref="state")
+    
+    def __init__(self, *args, **kwargs):
+        """
+        init inherited
+        """
+        super().__init__(*args, **kwargs)
 
     @property
     def cities(self):
