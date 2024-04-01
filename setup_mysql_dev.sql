244d45de6a43b28c70aab5d8cf7b1d0a029d3ad6
MrNeonn75 <devneonn75@gmail.com>
2024-04-02 02:15:35 +0400

    6. DBStorage - States and Cities Bug fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,5 +5,5 @@ SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-CREATE TABLE IF NOT EXISTS states;
-CREATE TABLE IF NOT EXISTS cities;
+CREATE TABLE IF NOT EXISTS hbnb_dev_db.states;
+CREATE TABLE IF NOT EXISTS hbnb_dev_db.cities;
