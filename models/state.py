c0453cacd903a4f684bf27ef8b0ed61953ed76fa
MrNeonn75 <devneonn75@gmail.com>
2024-04-02 02:34:24 +0400

    6. DBStorage - States and Cities Bug fix

--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
-    __tablename__ = "states"
+    __tablename__ = 'hbnb_dev_db.states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
